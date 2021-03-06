ff132baef03da7ee423eed87990fb9b0eb8dc65a
Jose Castaneyra <jcastaneyra@gmail.com>
2021-03-06 00:26:49 -0600

    Add dotenv library

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAxrK7AO74uWR7SRbILmP44qf1OAAPPbw4';
-
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3/',
   params: {
     part: 'snippet',
     maxResults: 5,
-    key: KEY,
+    key: `${process.env.REACT_APP_API_KEY}`,
   },
 });
