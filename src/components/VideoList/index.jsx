bf8ee90a08b2b82717b7150969c6dc561e5f5072
Jose Castaneyra <jcastaneyra@gmail.com>
2021-04-07 03:42:58 -0500

    Formatting by linter

--- src/components/VideoList/index.jsx
+++ src/components/VideoList/index.jsx
@@ -18,38 +18,30 @@ const VideoList = ({ videos, emptyMessage = 'No result videos' }) => {
   };
 
   const showVideoList = (videoList) => {
-    return (
-      videoList.length === 0 ? (
-        <Styled.Error> {emptyMessage} </Styled.Error>
-      ) : (
-        videoList
-          .filter((item) => item.snippet)
-          .map((item) =>
-            item.id.videoId ? { ...item, id: item.id.videoId } : { ...item }
-          )
-          .map((item) => (
-            <Styled.VideoScreenshotContainer key={item.id}>
-              <VideoCard
-                to={`/player/${item.id}`}
-                onClick={() => showVideoDetail(item)}
-                title={item.snippet.title}
-                description={item.snippet.description}
-                url={item.snippet.thumbnails.medium.url}
-              />
-            </Styled.VideoScreenshotContainer>
-          ))
-      )
-    )
-  }
+    return videoList.length === 0 ? (
+      <Styled.Error> {emptyMessage} </Styled.Error>
+    ) : (
+      videoList
+        .filter((item) => item.snippet)
+        .map((item) => (item.id.videoId ? { ...item, id: item.id.videoId } : { ...item }))
+        .map((item) => (
+          <Styled.VideoScreenshotContainer key={item.id}>
+            <VideoCard
+              to={`/player/${item.id}`}
+              onClick={() => showVideoDetail(item)}
+              title={item.snippet.title}
+              description={item.snippet.description}
+              url={item.snippet.thumbnails.medium.url}
+            />
+          </Styled.VideoScreenshotContainer>
+        ))
+    );
+  };
 
   return (
     <Styled.Container data-testid="content">
       {/* eslint-disable no-nested-ternary */}
-      {loading ? (
-        <LoadingSpinner />
-      ) : 
-        showVideoList(videos)
-      }
+      {loading ? <LoadingSpinner /> : showVideoList(videos)}
     </Styled.Container>
   );
 };
